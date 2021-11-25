4e2382b36938342e2f7fa8c70d3631ba1fd1022b
rzegnam <rzegnam@gmail.com>
2021-11-25 20:10:22 +0100

    first commit

---
+++ exercise_001.py
@@ -0,0 +1,7 @@
+# Create two variables, var1 and var2, both with the same value.
+var1 = 2
+var2 = 2
+
+# Create two variables, num1 and num2, which multiply together to give 16.
+num1 = 2
+num2 = 8
